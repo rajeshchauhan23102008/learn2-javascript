03ed75e651043facf5c757bdf263e25dc98670ce
“rajeshchauhan23102008” <rajesh.chauhan23102008@gmail.com>
2020-04-05 22:19:16 +0530

    changes commit

--- basics/app.js
+++ basics/app.js
@@ -1,3 +1,27 @@
-var myname = 'javascript';
+// Backlit character example
 
-var secondVar = 'secondvar';
+let name = 'ramesh';
+let age = 33;
+
+console.log(name + ' your age is' + age);
+
+console.log(`${name} your age is ${age}`);
+
+// function print() {
+//   if (true) {
+//     var age = 23;
+//     let newage = 34;
+
+//     newage = 45;
+
+//     const PI = 3.14;
+
+//     // PI = 10;
+
+//     console.log(newage);
+//     console.log(PI);
+//   }
+
+//   console.log(age);
+//   // console.log(newage);
+// }
